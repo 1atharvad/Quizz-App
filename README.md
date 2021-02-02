8b70b580cb3fbe2ba62cdf158372f3fd2b5490a6
Atharva Sarvesh Devasthali <atharvadev@cybage.com>
2021-02-02 19:04:31 +0530

    Final updated to user page

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Mini project on Quizz application
 Finally everything is done
 Just few things here and there are remaining.
 Bye
+
+# Instructions
+While executing the project, cut paste the server file in another folder. Run the server and the web application in different folders, so that there are no disturbances while using the website.
+
+If not done, there is a 100% chance that the page will abruptly get reloaded causing unwanted operaions in the system. 
+
--- js/quiz.js
+++ js/quiz.js
@@ -1,350 +1,3 @@
-var currentQuestion = 0;
-var correctAnswers = 0;
-var iSelectedAnswer = [];
-var qlength = 0;
-var c;
-var t;
-var quizOver = false;
-var category_name;
-var questions = [];
-
-$(document).ready(function () {
-  category_name = window.location.search.split("?")[1].split("=")[1];
- 
-  clickEvent(category_name);
-  $(".quizContainer").show();
-  
-  hideScore();
-
-  //Ajax method to get json data
-  function clickEvent(id) {
-    $.ajax({
-      method: "GET",
-      url: " http://localhost:3000/" + id,
-      success: function (x) {
-       x.forEach((items) => {
-          questions.push(items);
-        });
-        qlength = questions.length;
-        c=60*qlength
-      },
-      complete: function () {
-        displayCurrentQuestion(currentQuestion);
-        //navButtons();
-        //timedCount();
-      }
-    });
-  }
-
-  $(document).on("click", ".questionNo button", function () {
-    currentQuestion = this.id;
-    displayCurrentQuestion(parseInt(currentQuestion));
-  
-  });
-
-  //}
-
-  // previous Question button
-  $(this)
-    .find(".preButton")
-    .on("click", function () {
-      if (!quizOver) {
-       currentQuestion--;
-        if (currentQuestion < questions.length) {
-          displayCurrentQuestion(currentQuestion);
-        }
-      }
-      if ($(".preButton").text() === "View Answer") {
-        currentQuestion = 0;
-        $(".question").text("");
-        $(".choiceList").hide();
-        viewResults();
-      }
-    });
-
-  // Next Question button
-  $(this)
-    .find(".nextButton")
-    .on("click", function () {
-      if (!quizOver) {
-        currentQuestion++;
-       if (currentQuestion < questions.length) {
-          displayCurrentQuestion(currentQuestion);
-        } 
-     
-      }
-      });
-
-  //update correct answers after select choice
-  $(document).on("click", ".quizContainer > .choiceList ", function () {
-    var val = $("input[type='radio']:checked").val();
-    if (val === questions[currentQuestion].correct_option) {
-      correctAnswers++;
-    }
-    if(val!==undefined){
-      $(`#${currentQuestion}`).addClass("answer_selected");
-    }
-
-    iSelectedAnswer[currentQuestion] = val;
-  });
-
-// submit button
-  $(".submitbtn").click(function () {
-    quizComplete();
-  });
-});
-
-function quizComplete(){
-  $(".quizContainer").css("margin-left","10%");
-  
-  if (window.matchMedia('(max-width: 700px)').matches) {
-    
-     $(".quizContainer").css("margin-left","1%");
-  }
-  if (window.matchMedia('(min-width:701px)and (max-width: 900px)').matches) {
-    
-     $(".quizContainer").css("margin-left","15%");
-  }
-  if (window.matchMedia('(min-width:901px)').matches) {
-   
-     $(".quizContainer").css("margin-left","20%");
-  }
-
-  displayScore();
-  $(".image").show();
-
-  if(correctAnswers===0){
-  
-    $(".image").attr("src","./images/shocked.png");
-  }
-  else if(correctAnswers>0 && correctAnswers<4){
-  
-    $(".image").attr("src","./images/sad.png");
-  } 
-  else if(correctAnswers>=4 && correctAnswers<7){
-    $(".image").attr("src","./images/smile.png");
-  }
-  else{
-    
-    $(".image").attr("src","./images/emoji.png");
-  }
-
-
-  //$(".image").hide();
-  $(".questionNav").hide();
-  quizOver = true;
-    $(".question").hide();
-    $(".choiceList").hide();
-    $(document).find(".preButton").text("View Answer");
-    $(".preButton").prop("disabled", false);
-    $(".submit").hide();
-    $("#timer").hide();
-    //$(document).find(".nextButton").text("Close");
-   $(".nextButton").hide();
-  
-    return false;
-}
-
-// update time
-function timedCount() {
-  var hours = parseInt(c / 3600) % 24;
-  var minutes = parseInt(c / 60) % 60;
-  var seconds = c % 60;
-  var result =
-    (hours < 10 ? "0" + hours : hours) +
-    ":" +
-    (minutes < 10 ? "0" + minutes : minutes) +
-    ":" +
-    (seconds < 10 ? "0" + seconds : seconds);
-  $("#timer").html(result);
-
-  if (c === 0) {
-    quizComplete();
-  }
-
-  c = c - 1;
-  t = setTimeout(function () {
-    timedCount();
-  }, 1000);
-}
-
-// Display question
-function displayCurrentQuestion(currentQuestion) {
-	// Handling the previous button
-	if (currentQuestion === 0) {
-		$(".preButton").prop("disabled", true);
-	} else {
-		$(".preButton").prop("disabled", false);
-	}
-
-	// Handling the next button
-	if (currentQuestion === qlength-1){
-		$(".nextButton").prop("disabled", true);
-	} else {
-		$(".nextButton").prop("disabled", false);
-	}
-	var question = questions[currentQuestion].question;
-	var img_url=questions[currentQuestion].image_url;
-	var choiceList = $(document).find(".quizContainer > .choiceList");
-	var questionClass = $(document).find(".quizContainer > .question");
-	$(questionClass).text((currentQuestion + 1) + " . " + question);
-	if(img_url.length>0){
-		$(".image").show();
-		$(".image").attr("src","./images/"+img_url)
-	}
-	else{
-		$(".image").hide();
-	}
-	$(choiceList).find("li").remove();
-	let choice1 = [];
-	var option_1 = questions[currentQuestion].option_1;
-	var option_2 = questions[currentQuestion].option_2;
-	var option_3 = questions[currentQuestion].option_3;
-	var option_4 = questions[currentQuestion].option_4;
-	choice1.push(option_1);
-	choice1.push(option_2);
-	choice1.push(option_3);
-	choice1.push(option_4);
-
-	for (var i = 0; i < 4; i++) {
-		$(
-		'<li><input type="radio" class="radio-inline" value=' +
-			(i + 1) +
-			' name="dynradio" />' +
-			" " +
-			choice1[i] +
-			"</li>"
-		).appendTo(choiceList);
-}
-}
-
-function viewResults() {
-  $(".image").hide();
-  $("#timer").hide();
-  $(".question").show();
-  $(".preButton").hide();
-    hideScore();
-  for (var j = 0; j < qlength; j++) {
-    var question = questions[currentQuestion].question;
-   var  choice = [];
-    var option_1 = questions[currentQuestion].option_1;
-    var option_2 = questions[currentQuestion].option_2;
-    var option_3 = questions[currentQuestion].option_3;
-    var option_4 = questions[currentQuestion].option_4;
-    choice.push(option_1);
-    choice.push(option_2);
-    choice.push(option_3);
-    choice.push(option_4);
-
-    $(".question").append(
-      "<div>" + question + '</div><ul id="result_ul" style="list-style:none;">'
-    );
-    for (var i = 0; i < 4; i++) {
-      if (parseInt(iSelectedAnswer[currentQuestion]) === i + 1) {
-        if (parseInt(questions[currentQuestion].correct_option) === i + 1) {
-          $(".question").append(
-            '<li style="border:2px solid green;margin-top:10px;">' +
-              " " +
-              choice[i] +
-              "</li>"
-          );
-        } else {
-          $(".question").append(
-            '<li style="border:2px solid red;margin-top:10px;">' +
-              " " +
-              choice[i] +
-              "</li>"
-          );
-        }
-      } else {
-        if (parseInt(questions[currentQuestion].correct_option) === i + 1) {
-          $(".question").append(
-            '<li style="border:2px solid green;margin-top:10px;">' +
-              " " +
-              choice[i] +
-              "</li>"
-          );
-        } else {
-          $(".question").append("<li>" + " " + choice[i] + "</li>");
-        }
-      }
-    }
-
-    $(".question").append("</ul><hr>");
-    currentQuestion++;
-  }
-  $(".question").append("<button class='question_close'>Close</button");
-}
-//show score after submit 
-function displayScore() {
-  $(document)
-    .find(".quizContainer > .result")
-    .text("You scored: " + correctAnswers + " out of: " + questions.length);
-  $(document).find(".quizContainer > .result").show();
-}
-
-//hide score
-function hideScore() {
-  $(document).find(".result").hide();
-}
-
-
-//post score data in user array of json
-
-$(document).on("click", ".question_close", function (e) {
-  var email = sessionStorage.getItem("userid");
-
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
-  today = dd + "/" + mm + "/" + yyyy;
-  $.ajax({
-    method: "GET",
-    url: "http://localhost:3000/user",
-    success: function (result) {
-      result.forEach(function (userData) {
-        if (email === userData.email) {
-          var $categoryData = JSON.parse(userData.category);
-
-          if ($categoryData[category_name] === undefined) {
-            $categoryData[category_name] = [];
-          }
-           $categoryData[category_name][$categoryData[category_name].length] = {
-            score: correctAnswers,
-            date: today,
-          };
-          userData["category"] = JSON.stringify($categoryData);
-          $.ajax({
-            method: "PUT",
-            url: "http://localhost:3000/user/" + userData.id,
-            data: userData,
-          });
-        }
-      });
-    },
-    complete: () => {
-      location.href = "./";
-    },
-  });
-  e.preventDefault();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var selectedOptions = {};
 
 function getCategory() {
@@ -412,6 +65,10 @@ function questionEvent(id, questions) {
 function startQuiz(questions) {
 	var questionId = 0;
 
+	// Hide result and score sections
+	$("#score-card").hide();
+	$("#display-result").hide();
+
 	// To start the time and assigning 1 minute per question
 	startTimer(60*questions.length, questions);
 	questionEvent(questionId, questions);
@@ -457,6 +114,7 @@ function startTimer(time, questions) {
 		// When timer is over stop the timer and exit
 		if (time === 0) {
 			clearInterval(timer);
+			viewScore(calculateScore(questions), questions);
 		}
 		time--;
 	}, 1000);
@@ -464,7 +122,7 @@ function startTimer(time, questions) {
 	// When the submit button is clicked the timer stops
 	$("#submitModal #submitTest").on("click", () => {
 		clearInterval(timer);
-		calculateScore(questions);
+		viewScore(calculateScore(questions), questions);
 	});
 }
 
@@ -495,7 +153,7 @@ function queryOption(option) {
 	var options = 4;
 	for (var i=1; i<=options; i++) {
 		// To remove the previously added choice option class
-        // If no choice option class, do not remove that class
+        // If no choice option class, do not remove any class
 		$(`#quiz-question #option_${i}`).removeClass(function() {
 			var $class = $(this).attr("class").split(" ").pop();
 			if ($class === "option-choice") {
@@ -527,49 +185,141 @@ function queryQuestion(id) {
 	});
 }
 
+// Calculates the score based on the the options selected by the user
 function calculateScore(questions) {
 	var score = 0;
 	Object.keys(selectedOptions).forEach((questionId) => {
 		if (questions[questionId-1].correct_option === selectedOptions[questionId]) {
-			score++;
+			score += 1;
+		}
+	});
+	return score;
+}
+
+// To view the result of that quiz and images to mock the user depending upon the score
+function viewScore(score, questions) {
+	// Hide the quiz and display only score card
+	$("#quiz-page").hide();
+	$("#score-card").show();
+
+	var grade = questions.length/10;
+	if (score === 0){
+		$(".box_image img").attr("src", "./images/shocked.png");
+	} else if (score < Math.round(4*grade)) {
+		$(".box_image img").attr("src", "./images/sad.png");
+	} else if (score < Math.round(7*grade)) {
+		$(".box_image img").attr("src", "./images/smile.png");
+	} else {
+		$(".box_image img").attr("src", "./images/emoji.png");
+	}
+
+	// Displays the result
+	$("#result").text(`${score}/${questions.length}`);
+	updateScore(score);
+
+	// Button click for display the questions and correct options along with the selected options
+	$("#score-card .viewBtn").on("click", () => {
+		displayResult(questions);
+		$("#display-result").toggle();
+	});
+
+	// Back button to the home page
+	$("#display-result .backBtn").on("click", () => {
+		location.href = "./";
+	});
+}
+
+// Updates the score of the current test taken in the database
+function updateScore(score) {
+	var category_name = $("#quiz-name").text();
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, "0");
+	var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+	var yyyy = today.getFullYear();
+	$.ajax({
+		method: "GET",
+		url: "http://localhost:3000/user",
+		success: function(result) {
+			result.forEach(function (userData) {
+				if (sessionStorage.getItem("userid") === userData.email) {
+					var $categoryData = JSON.parse(userData.category);
+			
+					if (Object.keys($categoryData).includes(category_name) === false) {
+						$categoryData[category_name] = [];
+					}
+					$categoryData[category_name].push({
+						score: score,
+						date: `${dd}/${mm}/${yyyy}`,
+					});
+					userData["category"] = JSON.stringify($categoryData);
+
+					$.ajax({
+						method: "PUT",
+						url: "http://localhost:3000/user/" + userData.id,
+						data: userData,
+					});
+				}
+			});
 		}
 	});
 }
 
-function viewScore() {
-	$("#quiz-page").hide();
+// To display the questions along with correct answer and selected option.
+function displayResult(questions) {
+	var question_struct = $("#quiz-page #questionaire").html();
 
-	//// To be done
-	if(correctAnswers===0){
-	
-	  $(".image").attr("src","./images/shocked.png");
+	$("#display-result #display-questions").html("");
+	questions.forEach((question) => {
+		var htmlTag = `<div class="resultQuestion" id="question-${question.id}">
+							<h2 class="mb-4">Question ${question.id}</h2>
+							${question_struct}
+						</div>`;
+		$("#display-result #display-questions").append(htmlTag);
+		$("#display-result #display-questions").append("<hr/>");
+
+		$(`#display-result #question-${question.id} .question`).text(question.question);
+		
+		// To display the options
+		var options = 4;
+		for (var i=1; i<=options; i++) {
+			$(`#display-result #question-${question.id} #option_${i}`).text(question[`option_${i}`]);
+		}
+
+		// To query the options according to the selected options
+		queryOptionResult(question)
+	});
+}
+
+// Remove previously added classes and add new classes depending upon the selected options
+function queryOptionResult(question) {
+	var options = 4;
+	for (var i=1; i<=options; i++) {
+		// To remove the previously added choice option, correct option or wrong option class
+        // If no option class, do not remove any class
+		$(`#display-result #question-${question.id} #option_${i}`).removeClass(function() {
+			var $class = $(this).attr("class").split(" ").pop();
+			if ($class === "option-choice" || $class === "correct-choice" || $class === "wrong-choice") {
+				return $class;
+			} else {
+				return "";
+			}
+		});
 	}
-	else if(correctAnswers>0 && correctAnswers<4){
-	
-	  $(".image").attr("src","./images/sad.png");
-	} 
-	else if(correctAnswers>=4 && correctAnswers<7){
-	  $(".image").attr("src","./images/smile.png");
-	}
-	else{
-	  
-	  $(".image").attr("src","./images/emoji.png");
-	}
-  
-  
-	//$(".image").hide();
-	$(".questionNav").hide();
-	quizOver = true;
-	  $(".question").hide();
-	  $(".choiceList").hide();
-	  $(document).find(".preButton").text("View Answer");
-	  $(".preButton").prop("disabled", false);
-	  $(".submit").hide();
-	  $("#timer").hide();
-	  //$(document).find(".nextButton").text("Close");
-	 $(".nextButton").hide();
-	
-	  return false;
-  }
+
+	// Searches whether any options are selected else verifies whether that option is correct or not
+	if (Object.keys(selectedOptions).includes(question.id.toString())) {
+		if (question.correct_option === selectedOptions[question.id]) {
+			$(`#display-result #question-${question.id} #option_${question.correct_option}`).addClass("correct-choice");
+			console.log(question.id, "correct");
+		} else {
+			$(`#display-result #question-${question.id} #option_${question.correct_option}`).addClass("option-choice");
+			$(`#display-result #question-${question.id} #option_${selectedOptions[question.id]}`).addClass("wrong-choice");
+			console.log(question.id, "wrong + info");
+		}
+	} /*else {
+		$(`#display-result #question-${question.id} #option_${question.correct_option}`).addClass("option-choice");
+		console.log(question.id, "info");
+	}*/ // Commented because not answered question's answers should not be revealed.
+}
 
 getCategory();
